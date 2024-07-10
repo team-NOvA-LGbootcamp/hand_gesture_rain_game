9bff8db7d7f7a3c954216f9bdbc6a2ab0594b592
binyf <binyf3640@gmail.com>
2024-07-10 07:52:13 +0000

    fix: adapted_model_name

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from rain_game import RainGame
 import pygame
 
 def main():
-    model_path = 'sample.tflite'
+    model_path = 'SignMNIST_Augmentation.tflite'
     hgr = HandGestureRecognition(model_path)
     
     # hgr.run()을 별도의 스레드에서 실행
